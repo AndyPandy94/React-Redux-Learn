e48d8b3b263eacca3c20ac586e054574b634662e
Andreas Achilleos <andreasachilleos1994@gmail.com>
2021-05-12 13:10:07 +0100

    completed first lesson

--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+// function getButtonText() {
+//   return 'Click on me!';
+// }
+
+
 // Create a react component
 
 // function() { or () => {
 
 const App = () => {
-    return <div>Hi There my friends! </div>;
+  const buttonText = 'Click me';
+  // if writing like below must call it on line 31 as {buttonText.text}
+  // const buttonText = { text: 'Click me'};
+
+
+  return (
+    <div>
+      {/*Use className and not class below*/}
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      {/*The syntax for html styling in jsx*/}
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {/*The getButtonText is replaced with the text from the function on lines 6-8*/}
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 
 
